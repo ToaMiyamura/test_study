d9602221ec414826f107d0f20ce9871331c26de3
ToaMiyamura <ayaka.setou@icloud.com>
2022-05-14 12:15:22 +0900

    テストを実装しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -8,5 +8,15 @@
 
 bool is_leap_year(int year)
 {
-	return true;
+	if (year % 4 == 0 && year % 100 != 0)
+	{
+		return true;
+	}
+
+	if (year % 400 == 0)
+	{
+		return true;
+	}
+
+	return false;
 }
